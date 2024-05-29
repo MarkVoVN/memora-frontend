24b695985d48ecd3a901776fa52a8d8b365fe6e2
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2024-05-29 22:46:44 +0700

    fix build issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { useErrorNotification } from "@/hooks/useErrorNotification";
 import { loginUser } from "@/lib/api/userAPI";
